5132f1e1a6ac6ff6848c892dd407c177039546d8
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-09-18 12:53:11 +0600

    fix: update featured story badge text to Featured and robust dash separator

--- resources/views/frontend/homePage/success.blade.php
+++ resources/views/frontend/homePage/success.blade.php
@@ -642,7 +642,7 @@
                             
                             @if($fVideoInfo)
                                 <div class="story-media-overlay">
-                                    <span class="story-badge-video"><i class="fas fa-play" style="font-size: 9px;"></i> {{ __('Watch Story') }}</span>
+                                    <span class="story-badge-video"><i class="fas fa-star" style="font-size: 9px;"></i> {{ __('Featured') }}</span>
                                     <div class="story-play-btn">
                                         <svg width="22" height="22" viewBox="0 0 24 24">
                                             <polygon points="6 3 20 12 6 21 6 3"></polygon>
@@ -689,7 +689,7 @@
                                                 
                                                 @if($videoInfo)
                                                     <div class="story-media-overlay">
-                                                        <span class="story-badge-video"><i class="fas fa-play" style="font-size: 9px;"></i> {{ __('Watch Story') }}</span>
+                                                        <span class="story-badge-video"><i class="fas fa-star" style="font-size: 9px;"></i> {{ __('Featured') }}</span>
                                                         <div class="story-play-btn">
                                                             <svg width="22" height="22" viewBox="0 0 24 24">
                                                                 <polygon points="6 3 20 12 6 21 6 3"></polygon>
